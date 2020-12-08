2f228b29a967ca8896b17ea0ed4a3d6a3e7ae0bb
rannymonita <rannymonita@gmail.com>
2020-12-08 16:50:43 +0000

    Updating Current Date

--- src/app.js
+++ src/app.js
@@ -30,19 +30,15 @@ let date = currentTime.getDate();
 let hour = currentTime.getHours();
 if (hour < 10) {
   hour = `0${hour}`;
-} else {
-  hour = hour + "";
 }
 let minute = currentTime.getMinutes();
 if (minute < 10) {
   minute = `0${minute}`;
-} else {
-  minute = minute + ``;
 }
 let newTime = `${hour}:${minute}`;
 
-let h4 = document.querySelector("#current-date");
-h4.innerHTML = `${day}, ${month} ${date}, ${newTime}`;
+let dateElement = document.querySelector("#current-date");
+dateElement.innerHTML = `${day}, ${month} ${date}, ${newTime}`;
 
 //API
 
